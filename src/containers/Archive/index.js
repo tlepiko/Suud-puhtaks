08b01d4eeeaac9d73bd7a16d249103aabee40958
tlepiko <60349843+tlepiko@users.noreply.github.com>
2021-12-21 19:30:42 +0200

    Lisatud arhiivi salvestamine
    Lisatud funktsioon arhiivi salvestamiseks .csv failina.

--- src/containers/Archive/index.js
+++ src/containers/Archive/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import db from "../../firebase";
 import { onSnapshot, query, collection, where, orderBy } from '@firebase/firestore';
-import { returnToMain, statusName } from '../../actions/functions';
+import { returnToMain, statusName, htmlToCSV } from '../../actions/functions';
+const array121 = [1, 2, 3, 4, 5];
 /**
 * @author
 * @function Archive
@@ -11,12 +12,13 @@ import { returnToMain, statusName } from '../../actions/functions';
 export const Archive = () => {
     const [questions, setQuestions] = useState([]);
     const roomCode = localStorage.getItem("roomCode");
+    var html = document.querySelector("table").outerHTML;
     useEffect(
         () =>
             onSnapshot(query(
                 collection(db, "events", roomCode, "questions"),
                 //erinevad status olekud: 1. posed, 2. readyforanswer, 3. unsuitable, 4. answered, 5. answerlater
-                where("status", "in", [1, 2, 3, 4, 5]),
+                where("status", "in", array121),
                 orderBy("created", "asc")),
                 (snapshot) =>
                     setQuestions(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
@@ -38,11 +40,18 @@ export const Archive = () => {
             <button type="button" onClick={returnToMain}>Tagasi</button>
             <div clas="App">
                 <h1>Siia ilmuvad kõik küsimused</h1>
+                <table>
+                    <tr>
+                        <th>Küsimus</th>
+                        <th>Staatus</th>
+                    </tr>
+                
                 {questions.map(question => (
-                    <div id={question.id} value={question.id} key={question.id}>{question.question}{statusName(question.status)}</div>
+                    <tr id={question.id} value={question.id} key={question.id}><td>{question.question}</td><td>{statusName(question.status)}</td></tr>
                 ))}
+                </table>
+                <button onClick={() => htmlToCSV(html, "kysimused.csv")}>Lae alla!</button>
             </div>
         </div>
-
     )
 }
